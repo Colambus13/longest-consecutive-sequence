052c076122330aee4398b7388f70675c653e986b
Pavel Shestak <pavelshestak29@gmail.com>
2018-10-07 01:19:53 +0300

    'ten_task'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    var maxValue = 1;
+    var counter = 1;
+
+    if (array.length === 0) return 0;
+    if (array.length === 1) return 1;
+
+    function ownSort(a, b){
+        if(a > b) return 1;
+        if(a < b) return -1;
+    }
+    array.sort(ownSort);
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] === array[i - 1]) continue;
+        if (array[i - 1] === array[i] - 1) {
+            counter++;
+        } else {
+            counter = 1;
+        }
+        maxValue = Math.max(counter, maxValue);
+    }
+    return maxValue;
 }
