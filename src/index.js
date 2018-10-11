77db57aeca8b9084a2965e05583de9065ab7ec2a
Pavel Shestak <pavelshestak29@gmail.com>
2018-10-11 16:36:56 +0300

    'small_restyling'

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-    var maxValue = 1;
-    var counter = 1;
+    let maxValue = 1;
+    let counter = 1;
 
     if (array.length === 0) return 0;
     if (array.length === 1) return 1;
 
-    function ownSort(a, b){
-        if(a > b) return 1;
-        if(a < b) return -1;
-    }
-    array.sort(ownSort);
+    array.sort((a, b) => a - b);
 
-    for (var i = 1; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
         if (array[i] === array[i - 1]) continue;
-        if (array[i - 1] === array[i] - 1) {
+        if (array[i - 1] === array[i] - 1){
             counter++;
-        } else {
+        }else {
             counter = 1;
         }
         maxValue = Math.max(counter, maxValue);
